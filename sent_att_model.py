64046e7b62d4c4cdd272a340b0aa02972d1a7d9b
bigorange-Petrichor <75014054+bigorange-Petrichor@users.noreply.github.com>
2021-03-12 15:12:31 +0800

    Update sent_att_model.py

--- sent_att_model.py
+++ sent_att_model.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 # from src.utils import matrix_mul, element_wise_mul
 
 class SentAttNet(nn.Module):
-    def __init__(self, sent_hidden_size=100, word_hidden_size=100, num_classes=14):
+    def __init__(self, sent_hidden_size=100, word_hidden_size=100):
         super(SentAttNet, self).__init__()
         self.LSTM = nn.LSTM(word_hidden_size, sent_hidden_size)
-        self.fc = nn.Linear( sent_hidden_size, num_classes)
+        self.fc = nn.Linear( sent_hidden_size, 1)
         self.fc1 = nn.Linear( sent_hidden_size,sent_hidden_size)
         self.fc2 = nn.Linear( sent_hidden_size , 1,bias =False)
 
@@ -18,7 +18,7 @@ class SentAttNet(nn.Module):
         weight = F.softmax(weight,0)
         weight = weight * f_output
         output = weight.sum(0)
-        output = torch.sigmoid(self.fc(output))
+        output = F.sigmoid(self.fc(output))
         return output
 
 
